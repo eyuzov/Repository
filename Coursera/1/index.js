26459c85c1b73d1dc8531ffbde9c2bc6762c7257
eyuzov <eyuzov@gmail.com>
2019-03-24 20:47:43 +0300

    clean

--- Coursera/1/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * В этом задании необходимо сложить два числа. Цель заданий — настроить окружение и запустить простую программу.
- * @param {Number} a Первое слагаемое
- * @param {Number} b Второе слагаемое
- * @returns {Number}
- */
-module.exports = function (a, b) {
-  return Number(a) + Number(b);
-};
